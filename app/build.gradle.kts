b42cf048234609fa90f74db3f77b44cf7d926dfa
wized2 <wa0026343@gmail.com>
2026-09-14 16:53:39 +0000

    feat: 2.5.1 recent files chip row under toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.code"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "2.5.0"
+        versionCode = 19
+        versionName = "2.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
