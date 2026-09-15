0676b45eba48cda5a176e19eb08fb02809e0c31e
wized2 <code@canvas.app>
2026-09-15 10:09:36 +0000

    chore: 2.5.3 about notes recent chips

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.code"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "2.5.2"
+        versionCode = 21
+        versionName = "2.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
