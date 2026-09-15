e555e06f26bd9f344080e880d71fcdd1a528dcf3
wized2 <wa0026343@gmail.com>
2026-09-15 17:48:58 +0000

    chore: v2.6.3 patch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.code"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.6.2"
+        versionCode = 25
+        versionName = "2.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
