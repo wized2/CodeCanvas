6623195b7ce0ddc8a58461a2bc96b70281340d3a
Mudsir Wazir <wa0026343@gmail.com>
2026-09-18 15:14:02 +0500

    chore: CodeCanvas 2.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.code"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "2.6.9"
+        versionCode = 32
+        versionName = "2.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
